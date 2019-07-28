4138ab3f761740db2ea8f17df43e36103116c280
Cleison Pereira de Sousa <cleison.sousa@cd.com>
2019-07-28 20:54:16 -0300

    add in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,17 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService {
+  createDb() {
+    const categories = [
+      { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
+      { id: 2, name: 'Saúde', description: 'Plano de saúde e Remédios' },
+      { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+      { id: 3, name: 'Salário', description: 'Recebimento de Sálarios' },
+      { id: 3, name: 'Freelas', description: 'Trabalhos com freelancer' }
+    ];
+
+    return {
+      categories
+    }
+  }
+}
